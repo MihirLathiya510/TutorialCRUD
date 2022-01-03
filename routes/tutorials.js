56751e0738cb76ac4e1fb709bc31513657b6ede5
Mihir <mihir.lathiya@techholding.co>
2022-01-03 18:44:13 +0530

    fix the minor changes added .gitignore

--- routes/tutorials.js
+++ routes/tutorials.js
@@ -323,16 +323,16 @@ const idLength = 8;
   }
 */
 
-
+//get
 router.get('/',tutorialController.getTutorial);
 router.get('/:id',tutorialController.findTutorial);
 router.get('/searchbytitle/:title',tutorialController.findByTitleTutorial);
 router.get('/getsorted/sorted',tutorialController.getSortedTutorial);
-
+//post
 router.post('/post',tutorialController.postTutorial)
-
+//put
 router.put("/put/:id",tutorialController.putTutorial);
-
+//delete
 router.delete("/delete/:id",tutorialController.deleteTutorial);
 
 module.exports=router;
