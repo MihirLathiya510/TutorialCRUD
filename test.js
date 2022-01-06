7f3dd3eeee217e93ad4c170865d6e79f0a14a2f1
Mihir <mihir.lathiya@techholding.co>
2022-01-06 16:47:22 +0530

    refactor(test.js): testing the cz

--- test.js
+++ test.js
@@ -3,3 +3,5 @@ const logger = require('./logger/prod-logger');
 logger.error(new Error('testing the winston'));
 logger.info('testing the winston');
 logger.warn('testing the winston');
+
+logger.info('test test test');
