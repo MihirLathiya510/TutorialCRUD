ab17ad383f50fcddfbb367da4c541fbfc097cb6b
Mihir <mihir.lathiya@techholding.co>
2022-01-06 16:13:43 +0530

    feat(winston): testing the winston logger

---
+++ logger/prod-logger.js
@@ -0,0 +1,11 @@
+const { createLogger, format, transports } = require('winston');
+
+const { combine, timestamp, errors, json } = format;
+
+const loggerProd = createLogger({
+  format: combine(timestamp(), errors({ stack: true }), json()),
+  defaultMeta: { service: 'user-service' },
+  transports: [new transports.Console()],
+});
+
+module.exports = loggerProd;
--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-console.log('helllo meet');
+const logger = require('./logger/prod-logger');
+
+logger.error(new Error('testing the winston'));
+logger.info('testing the winston');
+logger.warn('testing the winston');
